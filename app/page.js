7681045e9b6632d5bca2b2085146b0ac18c83d6a
Arjav Jain <arjavjain0703@gmail.com>
2025-03-21 18:06:58 +0000

    fuck tpyescript;

---
+++ app/page.js
@@ -0,0 +1,18 @@
+// pages/index.js
+import Head from 'next/head';
+import ThreeScene from './three';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Three.js with Next.js</title>
+        <meta name="description" content="A simple Three.js scene in Next.js" />
+      </Head>
+      <main>
+        <h1>Welcome to Three.js with Next.js!</h1>
+        <ThreeScene />
+      </main>
+    </div>
+  );
+}
