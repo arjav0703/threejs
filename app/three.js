12bdbf00e11ca89c05a171b1850f4d5f154999c2
Arjav Jain <arjavjain0703@gmail.com>
2025-03-21 19:15:28 +0000

    add sphere;

--- app/three.js
+++ app/three.js
@@ -10,27 +10,27 @@ const ThreeScene = () => {
     canvas.id = 'draw';
     document.body.appendChild(canvas);
 
-    // Set up the scene, camera, and renderer
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true});
-    const cube = new THREE.Mesh(geometry, material);
-
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe:true })
+    const Boxgeometry = new THREE.BoxGeometry(1, 1, 1);
+    const cube = new THREE.Mesh(Boxgeometry, material);
+    
+    const Spheregeometry = new THREE.SphereGeometry(1, 10, 10);
+    const sphere = new THREE.Mesh(Spheregeometry, material);
     cube.rotation.y = 0.5;
     cube.rotation.x = 0.5;
 
-    scene.add(cube);
-
-    camera.position.z = 3;
-
+    //scene.add(cube);
+    scene.add(sphere);
     const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
-    controls.dampingFactor =0.1;
+    controls.dampingFactor =0.001;
 
     function animate() {
       window.requestAnimationFrame(animate);
@@ -39,7 +39,6 @@ const ThreeScene = () => {
 
       renderer.render(scene, camera);
 
-      // Uncomment to animate the cube
       // cube.rotation.z += 0.01;
       // cube.rotation.y += 0.01;
       // cube.scale.z += 0.01;
